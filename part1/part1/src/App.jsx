6f91489449e3adc971ebd2e000a582ff80433b53
zeryan <zeryan.g@gmail.com>
2024-01-16 23:14:38 -0300

    part1 exercise 1.1
    Exercise 1.1: Course Information, step1

---
+++ part1/part1/src/App.jsx
@@ -0,0 +1,33 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header>
+        <h1>{course}</h1>
+      </Header>
+      <Content>
+        <p>
+          {part1} {exercises1}
+        </p>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+      </Content>
+      <Total>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      </Total>
+    </div>
+  )
+}
+
+export default App
