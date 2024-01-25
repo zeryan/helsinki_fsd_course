df0d4b8dd8404032007f2d0d2fbee896e56b910e
zeryan <zeryan.g@gmail.com>
2024-01-24 21:40:20 -0300

    Exercise 1.12*: anecdotes step1 - update
    Exercise 1.12*: anecdotes step1 - update

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,4 @@
+// We import the React library and the useState hook from 'react'
 import React, { useState } from 'react'
 
 const Button = ({ text, handleClick }) => {
@@ -5,11 +6,15 @@ const Button = ({ text, handleClick }) => {
 }
 
 
+// The StatisticLine component is a row of a table. It receives a text for the label and a value. 
+// If the value is not a number, it presents it directly, otherwise, it formats it to have up to 
+//2 decimal digits. This component is used in the Statistics component below.
+
+const StatisticLine = ({ text, value }) => {
 // a <p> tag (paragraph tag) is not valid inside a <tbody> tag. 
 // I used a <tr> (table row) and <td> (table data) instead, or 
 // <th> (table header) tags inside a <tbody> tag.
 
-const StatisticLine = ({ text, value }) => {
   return (
     <tr>
       <td>{text}</td>
@@ -18,6 +23,9 @@ const StatisticLine = ({ text, value }) => {
   )
 }
 
+// The Statistics component renders a HTML table with the statistics of the feedback.
+// It calculates the total, average, and percentage of positive feedback.
+// If no feedback has been given yet, it shows a different message.
 const Statistics = ({good, neutral, bad}) => {
   const total = (good + neutral + bad);
   const average = (good - bad) / total;
@@ -48,6 +56,8 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
+// This is our main component, App. It renders buttons for each type of feedback and  
+// the statistics of the feedback. It uses useState to keep track of the count for each type of feedback.
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
